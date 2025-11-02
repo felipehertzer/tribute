03d8e744259315d2f7806e5d00a7c78ffeaf516e
Takashi Kato <tohosaku@users.osdn.me>
2025-11-02 17:13:18 +0900

    chore: Update dependencies

--- dagger.sh
+++ dagger.sh
@@ -1,7 +1,7 @@
-image=mcr.microsoft.com/playwright:v1.52.0-noble
+image=mcr.microsoft.com/playwright:v1.56.1-noble
 
 container |
-  from  $image |
+  from $image |
   with-directory /src .|
   with-workdir /src |
   with-exec npm install |
